cfd4e931a3892dd816a3e930e39bf8b2eaf2f9a4
ngtinh <nguyenvanductinh77@gmail.com>
2022-10-07 21:52:05 +0700

    [1] app nghe nhac

---
+++ app_wmp/app_wmp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace app_wmp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.oFD = new System.Windows.Forms.OpenFileDialog();
+            this.WMP = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(25, 26);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(172, 57);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play ...";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // oFD
+            // 
+            this.oFD.Filter = "Nhạc|*.mp3|Phim|*.mp4|All File|*.*";
+            this.oFD.FileOk += new System.ComponentModel.CancelEventHandler(this.oFD_FileOk);
+            // 
+            // WMP
+            // 
+            this.WMP.Enabled = true;
+            this.WMP.Location = new System.Drawing.Point(25, 108);
+            this.WMP.Name = "WMP";
+            this.WMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP.OcxState")));
+            this.WMP.Size = new System.Drawing.Size(795, 506);
+            this.WMP.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 644);
+            this.Controls.Add(this.WMP);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "MainForm";
+            this.Text = "Windows Media Player";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.OpenFileDialog oFD;
+        private AxWMPLib.AxWindowsMediaPlayer WMP;
+    }
+}
+
---
+++ app_wmp/app_wmp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_wmp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+
+            //Lay thong tin ok hay cancel
+            
+            DialogResult DR = oFD.ShowDialog();
+            if (DR == DialogResult.OK)
+            {
+                string url = oFD.FileName;
+                this.WMP.URL = url;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oFD_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
